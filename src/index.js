d71632641d282bf8ad30188ff7952a109dc6fc53
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-07-05 20:09:11 +0430

    add hashrouter in index.js file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
-<HashRouter basename="/blogwebsite-react"/>
+<HashRouter basename="https://nafasebra.github.io/blogwebsite-react/"/>
 
 ReactDOM.render(
   <React.StrictMode>
