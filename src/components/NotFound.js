12de61f3827bb83099e1d76191c18b0c0fb25182
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-07-05 10:48:04 +0430

    create 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 
 export default function NotFound() {
     return (
-        <div className="text-center p-8">
-            <h2 className="font-mono text-7xl sm:text-6xl md:text-7xl lg:text-8xl ">404</h2>
-            <h3 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl">Not found</h3>
-            <Link to="/" className="font-bold pt-4">Go to home :)</Link>
+        <div className="flex flex-col justify-center items-center" style={{height: '80vh'}}>
+            <h2 className="font-mono pb-0 text-7xl sm:text-6xl md:text-7xl lg:text-9xl ">404</h2>
+            <h3 className="font-mono font-thin pt-2 pb-7 text-3xl sm:text-2xl md:text-3xl lg:text-4xl">Not found</h3>
+            <Link to="/" >Go to home :)</Link>
         </div>
     )
 }
