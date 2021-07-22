3af742654e06ab3beb8903235311ada634ec3960
Nafiseh Ebrahimi <ebranafas@gmail.com>
2021-07-22 18:40:40 +0430

    add addnewpost.js file

---
+++ src/components/AddNewPost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddNewPost() {
+    return (
+        <>
+            <h3 className="p-10 text-center">Add new post</h3>
+        </>
+    )
+}
+
+export default AddNewPost
