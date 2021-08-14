e1aeb9f8a87222d68fb388249cbba242acfd8845
unknown <ebranafas@gmail.com>
2021-08-14 13:32:49 +0430

    add other post in postdata file |

--- src/data/postData.js
+++ src/data/postData.js
@@ -23,8 +23,7 @@ export const postData = [
                         Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. 
                         Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. O dio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. 
                         Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.`
-    },
-    {
+    }, {
         postID: 2,
         URLImage: img2,
         author: 'nafasebra',
@@ -42,14 +41,67 @@ export const postData = [
                         Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. 
                         Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. O dio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. 
                         Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.`
-    },
-    {
+    }, {
         postID: 3,
         URLImage: img3,
         author: 'nafasebra',
         date: 'Thursday, 8 July 2021',
         headText: 'Nature3',
         tags: ['lake', 'mountain'],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        longDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Egestas purus viverra accumsan in nisl nisi.
+                        Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. 
+                        In egestas erat imperdiet sed euismod nisi porta lorem mollis. 
+                        Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. 
+                        Sapien faucibus et molestie ac feugiat sed lectus vestibulum. 
+                        Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. 
+                        Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. 
+                        Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. O dio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. 
+                        Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.`
+    }, {
+        postID: 4,
+        URLImage: img1,
+        author: 'nafasebra',
+        date: 'Tuesday, 6 July 2021',
+        headText: 'Nature4',
+        tags: ['photography', 'winter', 'flower'],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        longDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Egestas purus viverra accumsan in nisl nisi.
+                        Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. 
+                        In egestas erat imperdiet sed euismod nisi porta lorem mollis. 
+                        Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. 
+                        Sapien faucibus et molestie ac feugiat sed lectus vestibulum. 
+                        Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. 
+                        Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. 
+                        Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. O dio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. 
+                        Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.`
+    }, {
+        postID: 5,
+        URLImage: img2,
+        author: 'nafasebra',
+        date: 'Wednesday, 7 July 2021',
+        headText: 'Nature5',
+        tags: ['summer', 'photography'],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+        longDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Egestas purus viverra accumsan in nisl nisi.
+                        Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. 
+                        In egestas erat imperdiet sed euismod nisi porta lorem mollis. 
+                        Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. 
+                        Sapien faucibus et molestie ac feugiat sed lectus vestibulum. 
+                        Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. 
+                        Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. 
+                        Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. O dio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. 
+                        Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.`
+    }, {
+        postID: 6,
+        URLImage: img3,
+        author: 'nafasebra',
+        date: 'Thursday, 8 July 2021',
+        headText: 'Nature6',
+        tags: ['mountain'],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
         longDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Egestas purus viverra accumsan in nisl nisi.
